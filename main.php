c84a924ca376806c75d0949df17d987c830e191d
lidy <191685963@qq.com>
2021-04-01 12:14:55 +0800

    main添加说明

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 <?php
-//include "StackInfo/LinkedListStack.php";
-
+/**
+ * 测试入口文件
+ * 在这里直接引用需要的类即可
+ * 示例：这里演示链表类的使用
+ */
 include "QueueInfo/LinkedListQueue.php";
 include "LinkedInfo/LinkedList.php";
 
